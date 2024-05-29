1ef85fba953de03baa1fd8b1565e4b3f0f4eb3d2
Lukecolman <79427199+Lukecolman@users.noreply.github.com>
2024-05-29 15:47:57 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+    <div>
+    <h1 className="text-3xl bg-red-300 font-bold underline">
+      BRANDGESTIC
+    </h1>
+    </div>
+
+    </>
+  )
+}
+
+export default App
