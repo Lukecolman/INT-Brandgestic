85092c1f9527b205faf3084f9f2e8641e9ed7530
Lukecolman <79427199+Lukecolman@users.noreply.github.com>
2024-05-30 13:10:10 -0300

    Image container and image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,21 +4,20 @@ import React, { useEffect, useState } from 'react';
 const Header = ({ title, cta }) => {
 
   return (
-    <div className="font-madefor flex text-center bg-violet-400 h-full">
+    <div className="font-madefor flex text-center bg-octo-green-900 h-full">
 
-    <div className="w-full p-4 text-center px-20  bg-green-300">
+    <div className="w-full p-4 text-center px-20  ">
       <h1 className="font-madefor text-2xl font-bold" dangerouslySetInnerHTML={{ __html: title  }}></h1>
       <button className="mt-4 bg-blue-500 text-white py-2 px-4 rounded">{cta}</button>
     </div>
 
     {/* IMG CONTAINTER */}
-    <div className="w-[48%] min-w-[48%] text-center bg-octo-pink relative">
-
-        {/* PENTAGON CONTAINER */}
-        <div className='pentagon-container'>
+    <div className="w-[48%] min-w-[48%] text-center relative">
+              {/* PENTAGON CONTAINER */}
+              <div className='pentagon-container '>
             <div>
                 <div className='pentagon-title'>
-                    <p className='-rotate-90 font-extrabold bg-red-400'>COVERED</p>
+                    <p className='-rotate-90 font-black'>COVERED</p>
                 </div>
                 <div>
                     AMOUNT
@@ -26,8 +25,12 @@ const Header = ({ title, cta }) => {
             </div>
         </div>
 
+      <div className="image-container"	>
+      <img className="absolute bottom-0 w-full	" src="public\images\family-export.png" alt="" />
+
+      </div>
+
         {/* IMG */}
-        <img className="absolute bottom-0	" src="https://source.unsplash.com/random/1800x1800?sig=1" alt="" />
     </div>
 
     </div>
